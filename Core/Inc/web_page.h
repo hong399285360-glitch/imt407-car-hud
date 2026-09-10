2a771b66e62abe7e26ca163d8bae89c21ec4b136
hong399285360-glitch <hong399285360@gmail.com>
2026-09-10 13:54:02 +0800

    chore: remove generated web_page.h (构建时由 gen_web_page.py 自动生成)

--- Core/Inc/web_page.h
+++
@@ -1,9 +0,0 @@
-#ifndef WEB_PAGE_H
-#define WEB_PAGE_H
-
-/* 内嵌 HUD 前端页面 (源文件 WebUI/index.html, 由 Scripts/gen_web_page.py 生成)
-   改完 index.html 请重新运行: python Scripts/gen_web_page.py */
-extern const char web_page_html[];
-extern const unsigned int web_page_html_len;
-
-#endif
